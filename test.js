e1aa64890fb240dceb40c3e45e5a4dac704012c4
Pawel Czubachowski <76answer76@gmail.com>
2018-02-06 10:56:59 +0100

    second commit

--- test.js
+++ test.js
@@ -1,5 +1,22 @@
 import test from 'ava';
+import linearRegression from './';
+
 
 test('unicorns are truthy', t => {
-    t.is('unicorn', 'unicorn');
+
+    const points = [
+        [43, 99],
+        [21, 65],
+        [25, 79],
+        [42, 75],
+        [57, 87],
+        [59, 81]
+    ];
+
+    var line = linearRegression(points);
+
+    t.is(line, {
+        a: 65.1415715245131,
+        b: 0.38522498321020804
+    });
 });
